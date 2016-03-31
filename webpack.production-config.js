5c4094e13d37194f637018ef5ab54866db70e9fa
Evan Davis <evan.davis@tapjoy.com>
2016-03-30 22:59:18 -0400

    don't hot-reload prod

--- webpack.production-config.js
+++ webpack.production-config.js
@@ -1,20 +1,36 @@
 var path = require('path');
 var webpack = require('webpack');
-var config = require('./webpack.config');
 
-config.devtool = 'source-map';
-
-config.plugins = config.plugins.concat([
-  new webpack.optimize.UglifyJsPlugin({
-    minimize: true,
-    compress: {
-      warnings: false
-    }
-  })
-]);
-
-config.entry = ['./src/index'];
-
-config.output.path = path.join(__dirname, 'static');
-
-module.exports = config;
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index',
+  output: {
+    path: path.join(__dirname, 'static'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    // polyfill fetch and promise: https://gist.github.com/Couto/b29676dd1ab8714a818f
+    new webpack.ProvidePlugin({
+      'Promise': 'es6-promise',
+      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'sass']
+    }]
+  }
+};

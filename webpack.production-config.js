f4c29ca2249d4134907b41e2aab7a8281ec1bbf0
Evan Davis <evan.davis@tapjoy.com>
2016-03-30 22:41:11 -0400

    correctly reference config file

--- webpack.production-config.js
+++ webpack.production-config.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
-var config = require('webpack.config');
+var config = require('./webpack.config');
 
 config.devtool = 'source-map';
 
